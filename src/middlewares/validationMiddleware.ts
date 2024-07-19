bf54c0d2cb64bd76281ec87c688d4dcef56dee5b
ovua <i.puddighinu@thecove.it>
2024-07-19 14:47:30 +0200

    fix/ validation + add/ nodemon

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,18 +1,36 @@
-import { Request, Response, NextFunction } from "express";
-import Joi from "joi";
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
 
-// Middleware validation for creating a new user
-export const validateCreateUser = [
-  {
-    name: Joi.string().max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).required,
-  },
-];
+const validateCreateUser = (req: Request, res: Response, next: NextFunction) => {
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required()
+    });
 
-// Middleware validation for updating a user
-export const validateUpdateUser = [{
-    name: Joi.string().max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(8).required,
-}];
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    next();
+};
+
+const validateUpdateUser = (req: Request, res: Response, next: NextFunction) => {
+    const schema = Joi.object({
+        name: Joi.string().optional(),
+        email: Joi.string().email().optional(),
+        password: Joi.string().min(6).optional()
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    next();
+};
+
+export { validateCreateUser, validateUpdateUser };
